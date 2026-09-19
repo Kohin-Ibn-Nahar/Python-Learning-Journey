632e9874785cce860a02cca1dc4579a0604dcd13
Sheikh Kohinoor Rahman <160453453+Kohin-Ibn-Nahar@users.noreply.github.com>
2026-09-19 20:33:13 +0600

    Add rectangle square check program
    Implement a program to check if a rectangle is a square based on user input for length and breadth.

---
+++ Session_2_Problem_4.py
@@ -0,0 +1,10 @@
+'''
+Take values of length and breadth of a rectangle from user and check if it is square or not.
+'''
+length = float(input("Enter Length: "))
+breadth = float(input("Enter Breadth: "))
+
+if length == breadth:
+    print("Square")
+else:
+    print("Not Square")

2de55861115a5ff7be01e4ff71f8991875ff62a2
Sheikh Kohinoor Rahman <160453453+Kohin-Ibn-Nahar@users.noreply.github.com>
2026-09-19 20:37:47 +0600

    Add program to find the greatest of two numbers

---
+++ Session_2_Problem_5.py
@@ -0,0 +1,9 @@
+#Take two int values from user and print greatest among them.
+
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+if num1>num2:
+    print("Greatest number is : ",num1)
+else:
+    print("Greatest number is : ",num2)

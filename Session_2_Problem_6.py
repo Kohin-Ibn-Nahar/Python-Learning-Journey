f845d73964b88a2634757b197269386fd812b021
Sheikh Kohinoor Rahman <160453453+Kohin-Ibn-Nahar@users.noreply.github.com>
2026-09-19 20:47:26 +0600

    Add discount logic for total cost calculation
    Implement discount calculation for purchases over 1000.

---
+++ Session_2_Problem_6.py
@@ -0,0 +1,12 @@
+'''
+A shop will give discount of 10% if the cost of purchased quantity is more than 1000. 
+Ask user for quantity Suppose, one unit will cost 100. Judge and print total cost for user.
+'''
+q = float(input("Enter Quantity : "))
+
+t = q*100
+
+if t>1000:
+    t = t- (t*10/100)
+
+print("Total Cost = ",t)
